42099f8d2d22be1887ed3f32b129143b2b6339d4
nickmattiacci <nickmattiacci@Nicks-MacBook-Pro.local>
2017-11-30 09:28:57 -0600

    Ready to run

--- request.js
+++ request.js
@@ -5,7 +5,8 @@ const identities = require('./utils/identities');
 const captcha = require('./utils/captcha');
 const request = require('request').defaults({
     followAllRedirects: true,
-    jar: true
+    jar: true,
+    proxy: 'http://Dille308:242DILLE@us43.coppedproxies.com:33128'
 });
 const cheerio = require('cheerio');
 const util = require('util');
@@ -14,7 +15,7 @@ const querystring = require('querystring');
 
 let findLink = (task, callback) => {
 
-    log.info(`Searching for ${task.style} ${task.keyword} in ${task.category}...`);
+    log.info(`Searching for ${task.style} ${task.keyword} in ${task.category}...`, task.nickname);
 
     let itemFound = false;
 
@@ -31,18 +32,18 @@ let findLink = (task, callback) => {
                 let color = $(this).children().children().last().text();
                 if (title.includes(task.keyword) && color.includes(task.style)) {
                     itemFound = true;
-                    log.success(`${color} ${title} found!`);
+                    log.success(`${color} ${title} found!`, task.nickname);
                     task.link = link;
                     task.title = title;
                     return callback(task);
                 } else if (title.includes(task.keyword) && color.includes(task.style) && $(this).children().text().indexOf('sold') != -1) {
                     itemFound = true;
-                    log.warning(`${color} ${title} is sold out. Waiting for restock...`);
+                    log.warning(`${color} ${title} is sold out. Waiting for restock...`, task.nickname);
                     return setTimeout(findLink, config.refreshRate, task, callback);
                 }
             });
             if (!itemFound) {
-                log.warning(`Could not find any items matching ${task.style} ${task.keyword} searching again in ${config.refreshRate / 1000} seconds...`);
+                log.warning(`Could not find any items matching ${task.style} ${task.keyword} searching again in ${config.refreshRate / 1000} seconds...`, task.nickname);
                 return setTimeout(findLink, config.refreshRate, task, callback);
             }
         }
@@ -50,7 +51,7 @@ let findLink = (task, callback) => {
 }
 
 let getCodes = (task, callback) => {
-    log.info(`Getting size and style codes for ${task.color} ${task.title}...`);
+    log.info(`Getting size and style codes for ${task.color} ${task.title}...`, task.nickname);
     request({
         url: `http://www.supremenewyork.com${task.link}`
     }, (err, resp, body) => {
@@ -63,17 +64,17 @@ let getCodes = (task, callback) => {
             task.atcLink = $('form[method=post]').attr('action');
             task.styleCode = $('#st').attr('value');
             if ($('b.sold-out').text() == 'sold out') {
-                log.warning(`${style} ${title} is sold out. Waiting for restock...`);
+                log.warning(`${style} ${title} is sold out. Waiting for restock...`, task.nickname);
                 return setTimeout(getCodes, config.refreshRate, task, callback);
             }
             if (task.size == '') {
                 item.sizeCode = $('#s').attr('value');
-                log.success(`Found size code: ${task.sizeCode} and style code: ${task.styleCode} for ${task.size} ${task.style} ${task.title}!`)
+                log.success(`Found size code: ${task.sizeCode} and style code: ${task.styleCode} for ${task.size} ${task.style} ${task.title}!`, task.nickname)
                 return callback(task);
             } else {
                 $('option').each(function(i, elem) {
                     if ($(this).text() == task.size) {
-                        log.success(`Found size code: ${task.sizeCode} and style code: ${task.styleCode} for ${task.size} ${task.style} ${task.title}!`)
+                        log.success(`Found size code: ${task.sizeCode} and style code: ${task.styleCode} for ${task.size} ${task.style} ${task.title}!`, task.nickname)
                         task.sizeCode = $(this).attr('value');
                         return callback(task);
                     }
@@ -84,7 +85,7 @@ let getCodes = (task, callback) => {
 }
 
 let addToCart = (task, callback) => {
-    log.info(`Adding ${task.size} ${task.style} ${task.title} to cart...`);
+    log.info(`Adding ${task.size} ${task.style} ${task.title} to cart...`, task.nickname);
     request({
         url: `http://www.supremenewyork.com${task.atcLink}`,
         method: 'POST',
@@ -99,7 +100,7 @@ let addToCart = (task, callback) => {
             log.error(util.inspect(err));
         } else {
             task.cookies = resp.request.headers.cookie
-            log.success(`Added ${task.size} ${task.style} ${task.title} to cart!`);
+            log.success(`Added ${task.size} ${task.style} ${task.title} to cart!`, task.nickname);
             return callback(task);
         }
     });
@@ -116,22 +117,22 @@ let checkout = (task) => {
             let csrf = $('[name="csrf-token"]').attr('content');
             let asec = $('input#asec').attr('value');
             let sitekey = $('div.g-recaptcha').attr('data-sitekey');
-            log.info(`Waiting for captcha response....`);
+            log.info(`Waiting for captcha response....`, task.nickname);
             if (config.captcha == 'open') {
                 captcha.open(sitekey, 0, (capResp) => {
-                    log.success(`Received captcha response ${capResp.substr(0, 10)}...`);
+                    log.success(`Received captcha response ${capResp.substr(0, 10)}...`, task.nickname);
                     setTimeout(post, config.checkoutDelay, task, capResp);
                 });
             }
             if (config.captcha == 'server') {
                 captcha.server(sitekey, 0, moment(), (capResp) => {
-                    log.info(`Received captcha response ${capResp.substr(0, 10)}...`);
+                    log.info(`Received captcha response ${capResp.substr(0, 10)}...`, task.nickname);
                     setTimeout(post, config.checkoutDelay, task, capResp);
                 });
             }
 
             function post(task, capResp) {
-                log.info(`Attempting to checkout ${task.size} ${task.style} ${task.title}...`);
+                log.info(`Attempting to checkout ${task.size} ${task.style} ${task.title}...`, task.nickname);
                 request({
                     url: 'https://www.supremenewyork.com/checkout.json',
                     method: 'POST',
@@ -162,7 +163,7 @@ let checkout = (task) => {
                 }, (err, resp, body) => {
                     var response = JSON.parse(body);
                     if (response.status == 'failed') {
-                        log.error(`Card decline error`);
+                        log.error(`Card decline error`, task.nickname);
                     } else {
                         console.log(body);
                     }
